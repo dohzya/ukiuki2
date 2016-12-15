912ecffbc723699f6545745bbf451cc3cad7d6e4
Étienne Vallette d'Osia <dohzya@gmail.com>
2016-12-15 10:56:04 +0900

    Clean code

--- blog.js
+++ blog.js
@@ -4,19 +4,19 @@ var configuration = require('./configuration').Configuration;
 exports.bloghome = function(req, res) {
   res.locals.config = configuration;
   var p = Prismic.withContext(req, res);
-  p.queryFirst('[[:d = at(document.type, "bloghome")]]', function (err, bloghome) {
+  p.queryFirst(Prismic.Predicates.at('document.type', 'bloghome'), function (err, bloghome) {
     if (err) { configuration.onPrismicError(err, req, res, 'get bloghome'); return; }
     if (bloghome) {
       var page = currentPage(req);
       var options = {
-        'page' : page,
-        'orderings' :'[my.post.date desc]'
+        page: page,
+        orderings: '[my.post.date desc]'
       };
-      p.query('[[:d = at(document.type, "post")]]', options, function (err, posts) {
+      p.query(Prismic.Predicates.at('document.type', 'post'), options, function (err, posts) {
         if (err) { configuration.onPrismicError(err, req, res, 'list posts'); return; }
         res.render('bloghome', {
-          'bloghome' : bloghome,
-          'posts' : posts.results
+          bloghome: bloghome,
+          posts: posts.results
         });
       });
     } else {
@@ -32,7 +32,7 @@ exports.post = function(req, res) {
   p.getByUID('post', uid, function then(err, post) {
     if (err) { configuration.onPrismicError(err, req, res, 'get post'); return; }
     if (post) {
-      res.render('post', {'post': post });
+      res.render('post', {'post': post});
     } else {
       res.status(404).send('Not found');
     }
